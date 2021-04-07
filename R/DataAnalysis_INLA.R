9d5d0b16f9aecf74d040e120bdf1f55283fabc2d
Aritz Adin <aritz.adin@unavarra.es>
2021-04-07 13:05:50 +0200

    Update file

--- R/DataAnalysis_INLA.R
+++ R/DataAnalysis_INLA.R
@@ -36,8 +36,8 @@ data$SMR <- data$obs/data$E
 # all.equal(sum(data$obs),sum(data$E))
 
 Data <- data.frame(O=data$obs, E=data$E, SMR=data$SMR,
-                   X1=scale(data$x1), # Sex ratio (nº of woman per 100,000 men)
-                   X2=scale(data$x2), # Population density (nº of people per square kilometer)
+                   X1=scale(data$x1), # Sex ratio (nÂº of woman per 100,000 men)
+                   X2=scale(data$x2), # Population density (nÂº of people per square kilometer)
                    X3=scale(data$x3), # Female literacy rate (%)
                    X4=scale(data$x4), # Per capita income (PCI)
                    X5=scale(data$x5), # Murder (per 100,000 people)
@@ -73,7 +73,7 @@ Qt <- t(Dm)%*%Dm
 carto_up <- readOGR("../data/carto_up")
 plot(carto_up,axes=T)
 
-## Add the neighborhood graph ##
+## Add the neighbourhood graph ##
 W <- -Qs
 diag(W) <- 0
 carto.nb <- mat2listw(W)$neighbours
@@ -88,12 +88,30 @@ sdunif="expression:
   return(logdens)"
 
 
+#############################################
+## Compute posterior pattern distributions ##
+#############################################
+compute.patterns <- TRUE  ## Set compute.patterns=FALSE if posterior patterns are not required
+
+if(compute.patterns){
+  source("posterior_lincombs.R")
+  all.lc <- all.lc
+  
+  source("posterior_lincombs_APredictor.R")
+  all.lc.Apredictor <- all.lc
+}else{
+  all.lc <- NULL
+  all.lc.Apredictor <- NULL
+}
+
+
 #########################################################################################################
 #########################################################################################################
 ## IMPORTANT NOTE: Uncomment and replace formula lines (f.M1-f.M4) for automatic definition when using ##
 ##                 your own data. In that case, the covariates should be named as X1, X2, ..., Xp.     ##                  ##
 #########################################################################################################
 #########################################################################################################
+strategy <- "simplified.laplace"
 
 #########################################
 ## Model ST1: Intercept + fixed-effect ##
@@ -104,49 +122,47 @@ f.M1 <- O ~ X1 + X2 + X3 + X4 + X5 + X6
 Model1 <- inla(f.M1, family="poisson", data=Data, E=E,
                control.predictor=list(compute=TRUE, cdf=c(log(1))),
                control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE),
-               control.inla=list(strategy="simplified.laplace"))
+               control.inla=list(strategy=strategy))
 
 Model1$summary.fixed
 
 
-#######################################################################
-## Model ST2: Intercept + random-effects (Eq. 4 -> with constraints) ##
-#######################################################################
+#########################################################################
+## Model ST2: Intercept + random-effects (Eq. 2.4 -> with constraints) ##
+#########################################################################
 
 ## Sum-to-zero constraints for the interaction ##
 R <- kronecker(Qt,Qs)
 r.def <- S+T-1
-Bst <- rbind(kronecker(t(ones.T),diag(S)),
-             kronecker(diag(T),t(ones.S)))
+Bst1 <- kronecker(t(ones.T),diag(S))
+Bst2 <- kronecker(diag(T),t(ones.S))
+Bst <- rbind(Bst1[-1,],Bst2[-1,])
 
 ## INLA model ##
 f.M2 <- O ~ X1 + X2 + X3 + X4 + X5 + X6 + 
   f(ID.area, model="besag", graph=Qs, constr=TRUE, rankdef=1, hyper=list(prec=list(prior=sdunif))) + 
   f(ID.year, model="rw1", constr=TRUE, rankdef=1, hyper=list(prec=list(prior=sdunif))) + 
   f(ID.area.year, model="generic0", Cmatrix=R, rankdef=r.def, hyper=list(prec=list(prior=sdunif)),
-    constr=TRUE, extraconstr=list(A=Bst, e=rep(0,S+T)))
+    constr=TRUE, extraconstr=list(A=Bst, e=nrow(Bst)))
 
 # f.M2 <- formula(paste("O ~",paste0(colnames(Data)[grep("^X", colnames(Data))],collapse="+"),
 #                       "+ f(ID.area, model='besag', graph=Qs, constr=TRUE, rankdef=1, hyper=list(prec=list(prior=sdunif)))",
 #                       "+ f(ID.year, model='rw1', constr=TRUE, rankdef=1, hyper=list(prec=list(prior=sdunif)))",
 #                       "+ f(ID.area.year, model='generic0', Cmatrix=R, rankdef=r.def, hyper=list(prec=list(prior=sdunif)),
-#                            constr=TRUE, extraconstr=list(A=Bst, e=rep(0,S+T)))"))
-
-source("posterior_lincombs.R")
-lc.M2 <- all.lc
+#                            constr=TRUE, extraconstr=list(A=Bst, e=nrow(Bst)))"))
 
 Model2 <- inla(f.M2, family="poisson", data=Data, E=E,
                control.predictor=list(compute=TRUE, cdf=c(log(1))),
                control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE),
-               lincomb=lc.M2,
-               control.inla=list(strategy="simplified.laplace"))
+               lincomb=all.lc,
+               control.inla=list(strategy=strategy))
 
 Model2$summary.fixed
 
 
-###################################################################
-## Model ST3a: Restricted regression (Eq. 8 -> with constraints) ##
-###################################################################
+#####################################################################
+## Model ST3a: Restricted regression (Eq. 3.2 -> with constraints) ##
+#####################################################################
 W <- diag(Model2$summary.fitted.values$mode*Data$E)
 W.sqrt <- diag(sqrt(diag(W)))
 
@@ -155,14 +171,18 @@ P <- W.sqrt%*%X%*%solve(t(X)%*%W%*%X)%*%t(X)%*%W.sqrt
 #rankMatrix(P)
 Pc <- diag(S*T)-P
 #rankMatrix(Pc)
-K <- as.matrix(eigen(P)$vectors[,eigen(P)$values>1e-12])
-L <- eigen(Pc)$vectors[,eigen(Pc)$values>1e-12]
+
+eigen.P <- eigen(P)
+eigen.Pc <- eigen(Pc)
+K <- as.matrix(eigen.P$vectors[,eigen.P$values>1e-12])
+L <- eigen.Pc$vectors[,eigen.Pc$values>1e-12]
 #rankMatrix(rbind(X,K))[1]==ncol(X)  # K is proportional to X
 #all(t(K)%*%L<1e-12)		# K'L=0
 
-Z.area <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt%*%kronecker(ones.T,diag(S))
-Z.year <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt%*%kronecker(diag(T),ones.S)
-Z.area.year <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt
+M <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt
+Z.area <- M%*%kronecker(ones.T,diag(S))
+Z.year <- M%*%kronecker(diag(T),ones.S)
+Z.area.year <- M%*%diag(S*T)
 
 Data.M3a <- list(O=Data$O, E=Data$E,
                  X1=Data$X1, X2=Data$X2, X3=Data$X3, X4=Data$X4, X5=Data$X5, X6=Data$X6,
@@ -193,9 +213,10 @@ Data.M3a <- list(O=Data$O, E=Data$E,
 ## Sum-to-zero constraints for the interaction ##
 R <- kronecker(Qt,Qs)
 r.def <- S+T-1
+Bst1 <- kronecker(t(ones.T),diag(S))
+Bst2 <- kronecker(diag(T),t(ones.S))
+Bst <- rbind(Bst1[-1,],Bst2[-1,])
 
-Bst <- rbind(kronecker(t(ones.T),diag(S)),
-             kronecker(diag(T),t(ones.S)))
 
 ## INLA model ##
 f.M3a <- O ~ -1 + Intercept + beta1 +  beta2 + beta3 + beta4 + beta5 + beta6 +
@@ -208,52 +229,55 @@ f.M3a <- O ~ -1 + Intercept + beta1 +  beta2 + beta3 + beta4 + beta5 + beta6 +
 #                        "+ f(ID.area, model='generic0', Cmatrix=Qs, rankdef=1, constr=TRUE, hyper=list(prec=list(prior=sdunif)))",
 #                        "+ f(ID.year, model='generic0', Cmatrix=Qt, rankdef=1, constr=TRUE, hyper=list(prec=list(prior=sdunif)))",
 #                        "+ f(ID.area.year, model='generic0', Cmatrix=R, rankdef=r.def, hyper=list(prec=list(prior=sdunif)),
-#                             constr=TRUE, extraconstr=list(A=Bst, e=rep(0,S+T)))"))
-
-source("posterior_lincombs_APredictor.R")
-lc.M3a <- all.lc
+#                             constr=TRUE, extraconstr=list(A=Bst, e=nrow(Bst)))"))
 
 Apredictor <- cbind(rep(1,S*T),Beta.df,Z.area,Z.year,Z.area.year)
 
 Model3a <- inla(f.M3a, family="poisson", data=Data.M3a, E=E,
                 control.predictor=list(compute=TRUE, A=Apredictor),
                 control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE),
-                control.inla=list(strategy="simplified.laplace"),
-                lincomb=lc.M3a,
+                control.inla=list(strategy=strategy),
+                lincomb=all.lc.Apredictor,
                 control.mode=list(theta=Model2$mode$theta, restart=FALSE),
                 control.fixed=list(prec=0))
 
 Model3a$summary.fixed
 
 
-##################################################################
-## Model ST3b: Restricted regression (Eq. 7 -> reparameterized) ##
-##################################################################
+####################################################################
+## Model ST3b: Restricted regression (Eq. 3.1 -> reparameterized) ##
+####################################################################
 W <- diag(Model2$summary.fitted.values$mode*Data$E)
 W.sqrt <- diag(sqrt(diag(W)))
 
-X <- cbind(ones.ST,Beta.df)
+X <- cbind(ones.ST,as.matrix(Beta.df))
 P <- W.sqrt%*%X%*%solve(t(X)%*%W%*%X)%*%t(X)%*%W.sqrt
 #rankMatrix(P)
 Pc <- diag(S*T)-P
 #rankMatrix(Pc)
-K <- as.matrix(eigen(P)$vectors[,eigen(P)$values>1e-12])
-L <- eigen(Pc)$vectors[,eigen(Pc)$values>1e-12]
+
+eigen.P <- eigen(P)
+eigen.Pc <- eigen(Pc)
+K <- as.matrix(eigen.P$vectors[,eigen.P$values>1e-12])
+L <- eigen.Pc$vectors[,eigen.Pc$values>1e-12]
 #rankMatrix(rbind(X,K))[1]==ncol(X)  # K is proportional to X
 #all(t(K)%*%L<1e-12)		# K'L=0
 
-Us <- eigen(Qs)$vectors[,eigen(Qs)$values>1e-12]
-Ds <- diag(eigen(Qs)$values[eigen(Qs)$values>1e-12])
+eigen.Qs <- eigen(Qs)
+Us <- eigen.Qs$vectors[,eigen.Qs$values>1e-12]
+Ds <- diag(eigen.Qs$values[eigen.Qs$values>1e-12])
 
-Ut <- eigen(Qt)$vectors[,eigen(Qt)$values>1e-12]
-Dt <- diag(eigen(Qt)$values[eigen(Qt)$values>1e-12])
+eigen.Qt <- eigen(Qt)
+Ut <- eigen.Qt$vectors[,eigen.Qt$values>1e-12]
+Dt <- diag(eigen.Qt$values[eigen.Qt$values>1e-12])
 
 Ust <- kronecker(Ut,Us)
 Dst <- kronecker(Dt,Ds)
 
-Z.area <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt%*%kronecker(ones.T,Us)
-Z.year <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt%*%kronecker(Ut,ones.S)
-Z.area.year <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt%*%Ust
+M <- solve(W.sqrt)%*%L%*%t(L)%*%W.sqrt
+Z.area <- M%*%kronecker(ones.T,Us)
+Z.year <- M%*%kronecker(Ut,ones.S)
+Z.area.year <- M%*%Ust
 
 Data.M3b <- list(O=Data$O, E=Data$E,
                  X1=Data$X1, X2=Data$X2, X3=Data$X3, X4=Data$X4, X5=Data$X5, X6=Data$X6,
@@ -287,25 +311,22 @@ f.M3b <- O ~ -1 + Intercept + beta1 +  beta2 + beta3 + beta4 + beta5 + beta6 +
   f(ID.year, model="generic0", Cmatrix=Dt, rankdef=0, constr=FALSE, hyper=list(prec=list(prior=sdunif))) +
   f(ID.area.year, model="generic0", Cmatrix=Dst, rankdef=0, constr=FALSE, hyper=list(prec=list(prior=sdunif)))
 
-source("posterior_lincombs_APredictor.R")
-lc.M3b <- all.lc
-
 Apredictor <- cbind(rep(1,S*T),Beta.df,Z.area,Z.year,Z.area.year)
 
 Model3b <- inla(f.M3b, family="poisson", data=Data.M3b, E=E,
                 control.predictor=list(compute=TRUE, A=Apredictor),
                 control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE),
-                control.inla=list(strategy="simplified.laplace"),
-                lincomb=lc.M3b,
+                control.inla=list(strategy=strategy),
+                lincomb=all.lc.Apredictor,
                 control.mode=list(theta=Model2$mode$theta, restart=FALSE),
                 control.fixed=list(prec=0))
 
-summary(Model3b)
+Model3b$summary.fixed
 
 
-#########################################################
-## Model ST4: Orthogonality constraints (Eq. 9 and 10) ##
-#########################################################
+############################################################
+## Model ST4: Orthogonality constraints (Eq. 3.3 and 3.4) ##
+############################################################
 W <- diag(Model2$summary.fitted.values$mode*Data$E)
 
 Bs <- rbind(t(ones.ST)%*%W%*%kronecker(ones.T,diag(S)),
@@ -317,6 +338,8 @@ Bt <- rbind(t(ones.ST)%*%W%*%kronecker(diag(T),ones.S),
 Bst <- rbind(t(Beta.df)%*%W,
              kronecker(t(ones.T),diag(S))%*%W,
              kronecker(diag(T),t(ones.S))%*%W)
+Bst <- Bst[-nrow(Bst),]
+
 
 R <- kronecker(Qt,Qs)
 r.def <- S+T-1
@@ -334,14 +357,11 @@ f.M4 <- O ~ X1 + X2 + X3 + X4 + X5 + X6 +
 #                        "+ f(ID.year, model='generic0', Cmatrix=Qt, rankdef=", p+1, ", constr=FALSE, hyper=list(prec=list(prior=sdunif)), extraconstr=list(A=Bt, e=rep(0,nrow(Bt))))",
 #                        "+ f(ID.area.year, model='generic0', Cmatrix=R, rankdef=",r.def+p+1, ", constr=FALSE, hyper=list(prec=list(prior=sdunif)), extraconstr=list(A=Bst, e=rep(0,nrow(Bst))))"))
 
-source("posterior_lincombs.R")
-lc.M4 <- all.lc
-
 Model4 <- inla(f.M4, family="poisson", data=Data, E=E,
                control.predictor=list(compute=TRUE, cdf=c(log(1))),
                control.compute=list(dic=TRUE, cpo=TRUE, waic=TRUE),
-               lincomb=lc.M4,
-               control.inla=list(strategy="simplified.laplace"))
+               lincomb=all.lc,
+               control.inla=list(strategy=strategy))
 
 Model4$summary.fixed
 
